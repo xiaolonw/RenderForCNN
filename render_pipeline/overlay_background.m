32765c811bb5def19f6872a70ceae02e73d58d2c
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-07-10 13:38:40 -0400

    render

--- render_pipeline/overlay_background.m
+++ render_pipeline/overlay_background.m
@@ -33,14 +33,20 @@ for i = 1:image_num
     catch
         fprintf('Failed to read %s\n', src_image_file);
     end   
+    
+    fprintf('%s\n', src_image_file); 
         
     s = size(I);
     fh = s(1); fw = s(2);
+    fprintf('%d %d\n', s(1), s(2)); 
+
+    s2 = size(alpha);
+    fprintf('%d %d\n', s2(1), s2(2)); 
+
+
     mask = double(alpha) / 255;
     mask = repmat(mask,1,1,3);
 
-    fprintf('%s\n', src_image_file); 
-    fprintf('%d %d\n', s(1), s(2)); 
     
     if rand() > clutteredBkgRatio
         I = uint8(double(I) .* mask + double(rand()*255) * (1 - mask));
