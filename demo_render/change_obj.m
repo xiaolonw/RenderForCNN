f3caffefff2241879043539163136e43cbda5cea
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-07-12 00:51:13 -0400

    demo

--- demo_render/change_obj.m
+++ demo_render/change_obj.m
@@ -23,12 +23,12 @@ for i = 1 : numel(list)
     	if length(tline) == 0
     		break;
     	end
-    	fprintf(fid2, '%s\n', tline); 
+    	fprintf(fid2, '%s', tline); 
     	if flag == 0
     		flag = 1;
-    		fprintf(fid, 'usemtl texture_optimized_tsdf_texture_mapped_mesh.png\n');
-    		fprintf(fid, 'o object_%d\n', i);
-    		fprintf(fid, 'g object_%d\n', i);
+    		fprintf(fid2, 'usemtl texture_optimized_tsdf_texture_mapped_mesh.png\n');
+    		fprintf(fid2, 'o object_%d\n', i);
+    		fprintf(fid2, 'g object_%d\n', i);
     	end
 
     end
