aafcd31234008452af82277545aa6729c1ae8b46
xiaolonw <xiaolonw@cs.cmu.edu>
2016-07-12 22:26:31 -0400

    global

---
+++ global_variables.m
@@ -0,0 +1,8 @@
+g_matlab_kde_folder = '/nfs.yoda/xiaolonw/grasp/RenderForCNN/render_pipeline/kde/matlab_kde_package';
+g_view_statistics_folder = '/nfs.yoda/xiaolonw/grasp/RenderForCNN/data/view_statistics';
+g_view_distribution_folder = '/nfs.yoda/xiaolonw/grasp/RenderForCNN/data/view_distribution';
+g_truncation_statistics_folder = '/nfs.yoda/xiaolonw/grasp/RenderForCNN/data/truncation_statistics';
+g_truncation_distribution_folder = '/nfs.yoda/xiaolonw/grasp/RenderForCNN/data/truncation_distribution';
+g_pascal3d_root_folder = '/nfs.yoda/xiaolonw/grasp/RenderForCNN/datasets/pascal3d';
+g_cls_names = {'aeroplane','bicycle','boat','bottle','bus','car','chair','diningtable','motorbike','sofa','train','tvmonitor'};
+g_detection_results_folder = '/nfs.yoda/xiaolonw/grasp/RenderForCNN/data/detection_results';
